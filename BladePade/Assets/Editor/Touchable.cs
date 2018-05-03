d2bd753fcb248e6d36bf23a02e4c165d857a6a8d
Mobot <po4tajurajkeee@gmail.com>
2018-05-03 17:56:06 +0300

    123123

---
+++ BladePade/Assets/Editor/Touchable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class Touchable : Text
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+}
+ 
+
+ [CustomEditor(typeof(Touchable))]
+public class Touchable_Editor : Editor
+{
+    public void OnInspectorGUI()
+    {
+        // Do nothing
+    }
+}
