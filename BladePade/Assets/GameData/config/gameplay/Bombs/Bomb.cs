8ed35c45e2b7862000d60a572141ca19317f131d
Longlife Games <po4tajurajkeee@gmail.com>
2018-06-20 13:18:36 +0300

    31232

---
+++ BladePade/Assets/GameData/config/gameplay/Bombs/Bomb.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    public PlayerStats playerMethodsAssembly;
+    public float delayBeforeExplosion;
+    private bool playerInRadiusOfExplosion;
+
+    private void Start()
+    {
+        StartCoroutine(Explode(delayBeforeExplosion));
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.transform.tag == "Player")
+            playerInRadiusOfExplosion = true;
+        else
+            playerInRadiusOfExplosion = false;
+    }
+    IEnumerator Explode(float seconds)
+    {
+        
+        yield return new WaitForSeconds(seconds);
+        if (playerInRadiusOfExplosion) playerMethodsAssembly.KillMe();
+        Destroy(this.gameObject);
+    }
+}
